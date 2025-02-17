be6a0a027882d50392580e512bdccf1468b70b87
Ankita Singh <ankitasinghthakur12@gmail.com>
2025-02-17 11:11:11 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = null
+// console.log(score);
+// console.log(typeof(score));
+
+ let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //NaN Not A Number
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true ==> 1; false ==> 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(isLoggedIn);
+
+// console.log(booleanIsLoggedIn );
+// 1==> true; 0 ==> false
+// "" ==> false
+//"Ankita" ==> true
+
+let your = 18
+let age = String(your);
+console.log(your);
+console.log(typeof age);
+
+
+
