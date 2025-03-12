fdd1b09dfb5a8afacc6444ce3a0bf0cd5b564517
Ankita Singh <ankitasinghthakur12@gmail.com>
2025-03-12 17:47:58 +0000

    Data type summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,30 @@
+//JavaScrpit is a dynamically typed language language
+
+/*Primitive datatypes-
+ 7 types- String, Number, Boolean, BigInt, null, undefined, Symbol
+*/
+
+/* Non-Primitive datatypes-
+ 3 types- Arrays, Objects, functions
+ */
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+ console.log(id === anotherId);
+ 
+ const bigNumber = 23143546768n
+
+ const heros = ["shaktiman", "nagraj", "doga"];
+ let myobj = {
+    name: "Ankita",
+    age: 18
+ }
+
+ const myFunction = function(){
+    console.log("Hello World");
+ }
+ console.log(typeof bigNumber);
+ console.log(typeof myFunction);
+ console.log(typeof heros);
+ 
+ 
