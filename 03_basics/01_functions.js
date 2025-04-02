11cff5edeb3ce8f76ac8615a3f07e49ce480e9df
Ankita Singh <ankitasinghthakur12@gmail.com>
2025-04-02 18:25:25 +0000

    new updates

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,5 +1,5 @@
 function sayMyName(){ //function definition
-    console.log("Hello This is a sunction");
+    //console.log("Hello This is a sunction");
 }
 
 sayMyName(); //'sayMyName' function reference with () this this function call
@@ -13,4 +13,35 @@ const result =  addTwoNumbers(3, 4); //function call with arguments
 function login(username){
     return`${username} is logged in`;
 }
-console.log(login("John")); //function call with arguments
+//console.log(login("John")); //function call with arguments
+
+
+function calculateCartPrice(val1, val2, ...num1){ 
+    return num1
+    // let total = 0; 
+    // for(let i = 0; i < num1.length; i++){
+    //     total += num1[i];
+    // }
+    // console.log(total);
+}
+//console.log(calculateCartPrice(200, 400, 500, 2000));
+ 
+const user = {
+    username: "hitesh",
+    price: 199
+}
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+//handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 499
+})
+
+const myNewArray = [1, 2, 3, 4]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray)); //function call with array
+console.log(returnSecondValue([1, 2, 5, 7]));
