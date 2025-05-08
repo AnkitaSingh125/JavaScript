fa302e73bf97ac8c158259a2e6a654be45f76249
Ankita Singh <ankitasinghthakur12@gmail.com>
2025-05-08 17:04:20 +0000

    Loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,10 @@
+//For loop
+for (let index = 0; index < 10; index++) {
+    if(index == 5){
+        console.log(`Detected 5`);
+        break; //Continue to skip the rest of the loop
+        
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
