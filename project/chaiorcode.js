6bc1a3966de42ffe5a968f4c8c095492640baf24
Ankita Singh <ankitasinghthakur12@gmail.com>
2025-07-14 16:41:41 +0000

    Color Switcher Project

--- project/chaiorcode.js
+++ project/chaiorcode.js
@@ -1,11 +1,42 @@
-const buttons = document.querySelectorAll('.button')
+/*const buttons = document.querySelectorAll('.button')
 //console.log(buttons);
 const body = document.querySelector('body')
 
 buttons.forEach(function(button){
     console.log(button);
-    button.addEventListener('click', function(e){
-        console.log(e)
-        console.log(e.target)
+    button.addEventListener('click', function(event){
+        console.log(event);
+        console.log(event.target);
+        
+        const color = getComputedStyle(button).backgroundColor;
+        document.body.style.backgroundColor = color;
+    
     })
-})
+});*/
+
+///////Second Way///////
+console.log("hitesh")
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    
+  });
+});
+
